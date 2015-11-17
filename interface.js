e56441bb2aa585bc7ca0e459a9bbc390bf9bd902
Brett Guillory <kurzweilfreak@gmail.com>
2015-11-17 00:11:20 -0600

    Begin to implement interface rendering/tracking
    Ported fillColorArray() function
    Ported changeBGColor() function
    Ported animateTicker() function

--- interface.js
+++ interface.js
@@ -1,3 +1,47 @@
 var interface = (function() {
 
+    var colorArray = fillColorArray();
+    var $background = $('.clock-inside');
+    var $minuteCounter = $('')
+    var $tickerSlider = $('.ticker');
+    
+    function fillColorArray(){
+        return randomColor({count: 100});
+    }
+    
+    function changeBGColor() {
+        if (colorArray.length < 1) {
+            fillColorArray();
+        }
+        $background.css('background-color', colorArray.shift());        
+    }
+    
+    function animateTicker() {
+        var tickerState = false;
+        var left = "0%";
+        var right = "95%";
+        function move(whichWay){
+            $tickerSlider.animate({
+                left: whichWay
+            }, 500, bgChange); //changes bgcolor at end of animation
+        }
+        return function() {
+            //tickerstate false = ticker on left
+            if (!tickerState) { 
+                tickerState = true;
+                move(right);
+            //tickerstate true = ticker on right
+            } else {
+                tickerState = false;
+                move(left);
+            } //end if/else
+        } //end return function
+    } //end animateTicker() 
+    
+    function updateMinutes(seconds) {
+        
+    }
+    
+    
+    eventTracker.on('counted', );
 });
