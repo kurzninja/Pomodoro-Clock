493cc38c09643a8f0f634279c1ee0ce285d5f1e2
Brett Guillory <kurzweilfreak@gmail.com>
2015-11-17 00:08:42 -0600

    Beginning to implement counter
    Filled out basic counter implementation for tracking time changes

--- counter.js
+++ counter.js
@@ -1,4 +1,21 @@
 var counter = (function() {
+    
+    var workTime = parseInt($('.work .number')).html(), 10);
+    var breakTime = parseInt($('.break .number')).html(), 10);
+    var startTime = workTime * 60;
+    var timeLeft = startTime;
+    
+    var tick = function() {
+        if (timeLeft > 0) {
+            timeLeft--;
+            eventTracker.emit('counted', timeLeft);
+        } 
+        else {
+            eventTracker.emit('timeup');
+        }
+            
+    }
 	
+    eventTracker.on('tick', tick);
 	
 });
