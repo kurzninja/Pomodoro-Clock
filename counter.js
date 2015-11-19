c2cfecbc3d3cb6245ca0a2fd48318304d06a214b
Brett Guillory <kurzweilfreak@gmail.com>
2015-11-19 17:22:28 -0600

    Muted console logging

--- counter.js
+++ counter.js
@@ -4,7 +4,7 @@ var Counter = function() {
     var timeLeft = "";
     
     var tick = function() {
-        console.log("counter.tick ticked", "timeLeft: " + timeLeft);
+//        console.log("counter.tick ticked", "timeLeft: " + timeLeft);
         if (timeLeft > 0) {
             timeLeft--;
             EventTracker.emit('counted', timeLeft);
