44d2e522b9ab2c1efb892cab49c06fb921a0d2b3
Brett Guillory <kurzweilfreak@gmail.com>
2015-12-01 16:30:48 -0600

    Changed emitting to pass data object
    Had to adjust emitting function handlers to accept data objects rather
    than individual data arguments

--- counter.js
+++ counter.js
@@ -3,41 +3,44 @@ Counter object constructor that keeps track of how much time is left, when time 
 */
 
 var Counter = function() {
-    
-    
+
+
     var workTimeLeft = "";
     var breakTimeLeft = "";
     var mode = true; //true = work, false = break;
-    
+
     var tick = function() {
-        if (mode) {  // if in work mode      
+        if (mode) {  // if in work mode
 //        console.log("counter.tick ticked", "timeLeft: " + timeLeft);
             if (workTimeLeft > 0) { // if time left in work mode
                 workTimeLeft--;
-                EventTracker.emit('counted', workTimeLeft, mode);
-            } 
+                data = {"timeLeft": workTimeLeft, "mode": mode};
+                EventTracker.emit('counted', data);
+            }
             else {   // work mode time runs out
                 mode = false; //switch to break mode
-                EventTracker.emit('counted', breakTimeLeft, mode); //transmit break start time
+                data = {"timeLeft": breakTimeLeft, "mode": mode};
+                EventTracker.emit('counted', data); //transmit break start time
             }
         } else { // if in break mode
             if (breakTimeLeft > 0) { // if time left in break mode
                 breakTimeLeft--;
-                EventTracker.emit('counted', breakTimeLeft, mode);                
-            } 
+                data = {"timeLeft": breakTimeLeft, "mode": mode};
+                EventTracker.emit('counted', data);
+            }
             else { // break mode time runs out
                 mode = true;
                 EventTracker.emit('timeup');
-            }            
-        }            
+            }
+        }
     }
-    
+
     EventTracker.on('init', function(initialState){
-        workTimeLeft = initialState.startTime * 60;        
+        workTimeLeft = initialState.startTime * 60;
         breakTimeLeft = initialState.breakTime * 60;
         EventTracker.emit('start', workTimeLeft);
     });
-	
+
     EventTracker.on('tick', tick);
-	
+
 };
